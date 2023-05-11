1e6d60481f64fded8886b4b5cb5e6e7ab460108e
malak271 <malakghabayen396@gmail.com>
2023-05-11 10:06:33 +0300

    add takning picture instructions

--- lib/screens/taking_picture_screen.dart
+++ lib/screens/taking_picture_screen.dart
@@ -1,15 +1,16 @@
+import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_cropper/image_cropper.dart';
 
 import '../shared/ast_cubit/cubit.dart';
 import '../shared/ast_cubit/states.dart';
 import '../shared/components/Constants.dart';
-import 'display_picture_screen.dart';
+
 
 class TakePictureScreen extends StatefulWidget {
-
   const TakePictureScreen({
     super.key,
     required this.camera,
@@ -26,6 +27,7 @@ class TakePictureScreenState extends State<TakePictureScreen> {
   bool showFocusCircle = false;
   double x = 0;
   double y = 0;
+  String text='line up the petri dish so that its edges touch the frame';
 
   @override
   void initState() {
@@ -47,51 +49,95 @@ class TakePictureScreenState extends State<TakePictureScreen> {
 
   @override
   Widget build(BuildContext context) {
+    var cubit = AppCubit.getCubit(context);
     if (!controller.value.isInitialized) {
       return Container();
     }
-    return  GestureDetector(
+    return GestureDetector(
         onTapUp: (details) {
           _onTap(details);
         },
         child: BlocConsumer<AppCubit, States>(
           listener: (context, state) async {
-            if(state is UploadImgSuccessState){
-              print("========================================");
-              await Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => DisplayPictureScreen(
-                    // Pass the automatically generated path to
-                    // the DisplayPictureScreen widget.
-                      imagePath: state.imagePath,
-                      // astModel:state.astModel
-                  ),
-                ),
-              );
-            }
+            // if(state is UploadImgSuccessState){
+            //   print("========================================");
+            //   await Navigator.of(context).push(
+            //     MaterialPageRoute(
+            //       builder: (context) => DisplayPictureScreen(
+            //         // Pass the automatically generated path to
+            //         // the DisplayPictureScreen widget.
+            //         //   imagePath: state.imagePath,
+            //           // astModel:state.astModel
+            //       ),
+            //     ),
+            //   );
+            // }
+            // if(state is CreateTestSuccessState){
+            //   print("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh");
+            //   Fluttertoast.showToast(
+            //     msg: "Insertion was successful",
+            //     toastLength: Toast.LENGTH_LONG,
+            //     gravity: ToastGravity.BOTTOM,
+            //     timeInSecForIosWeb: 1,
+            //     fontSize: 16,
+            //     backgroundColor: Colors.green,
+            //     textColor: Colors.white,
+            //   );
+            //   cubit.cropImage(test_id: cubit.testId!);
+            //   navigateTo(context, DisplayPictureScreen());
+            // }
           },
-          builder:(context,state)=> Scaffold(
+          builder: (context, state) => Scaffold(
             body: Stack(
               children: [
-                Center(
-                    child: CameraPreview(controller)
-                ),
-                if(showFocusCircle) Positioned(
-                    top: y-20,
-                    left: x-20,
-                    child: Container(
-                      height: 40,
-                      width: 40,
-                      decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: Border.all(color: Colors.white,width: 1.5)
-                      ),
-                    ))
+                Center(child: CameraPreview(controller)),
+                if (showFocusCircle)
+                  Positioned(
+                      top: y - 20,
+                      left: x - 20,
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border:
+                                Border.all(color: Colors.white, width: 1.5)),
+                      )),
+                Positioned.fill(
+                    child: Align(
+                        alignment: Alignment.center,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              width: 2,
+                              color: Colors.white
+                            ),
+                          ),
+                          alignment: Alignment.center,
+                          width: 375,
+                          height: 400,
+                          child:
+                          Container(
+                            height: 80,
+                            color: Colors.black.withOpacity(.5),
+                            alignment: Alignment.center,
+                            child: Text(
+                              text,
+                              style: TextStyle(color: Colors.white,),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ))),
               ],
             ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
             floatingActionButton: FloatingActionButton(
               // Provide an onPressed callback.
               onPressed: () async {
+                setState(() {
+                  text="Taking photo...";
+                });
                 // Take the Picture in a try / catch block. If anything goes wrong,
                 // catch the error.
                 try {
@@ -102,21 +148,43 @@ class TakePictureScreenState extends State<TakePictureScreen> {
                   // where it was saved.
                   final image = await controller.takePicture();
 
+print("4444444444444444");
+try{
+                  CroppedFile? croppedFile = await ImageCropper().cropImage(
+                      sourcePath: image.path,
+                      aspectRatioPresets: [
+                        CropAspectRatioPreset.square,
+                        CropAspectRatioPreset.ratio3x2,
+                        CropAspectRatioPreset.original,
+                        CropAspectRatioPreset.ratio4x3,
+                        CropAspectRatioPreset.ratio16x9
+                      ],
+                      uiSettings:[
+                        AndroidUiSettings(
+                          toolbarTitle: 'Cropper',
+                          toolbarColor: Colors.deepOrange,
+                          toolbarWidgetColor: Colors.white,
+                          initAspectRatio: CropAspectRatioPreset.original,
+                          lockAspectRatio: false),
+                        IOSUiSettings(
+                        minimumAspectRatio: 1.0,
+                      )
+                       ]
+                  );
+                     cubit.image_path = croppedFile!.path;
+                  AppCubit.getCubit(context).createNewTest(
+                      bacteria: cubit.bacteria!,
+                      sample_type: cubit.sampleType!,
+                      imagePath: image.path);
+
+}catch(error){
+  print(error);
+}
+print("55555555555555555");
+
                   if (!mounted) return;
 
-                  AppCubit.getCubit(context).uploadImage(imagePath: image.path);
-
-                  // If the picture was taken, display it on a new screen.
-                  // await Navigator.of(context).push(
-                  //   MaterialPageRoute(
-                  //     builder: (context) => DisplayPictureScreen(
-                  //       // Pass the automatically generated path to
-                  //       // the DisplayPictureScreen widget.
-                  //       imagePath: image.path,
-                  //     ),
-                  //   ),
-                  // );
-
+               
 
                 } catch (e) {
                   // If an error occurs, log the error to the console.
@@ -126,13 +194,11 @@ class TakePictureScreenState extends State<TakePictureScreen> {
               child: const Icon(Icons.camera_alt),
             ),
           ),
-        )
-    );
+        ));
   }
 
-
   Future<void> _onTap(TapUpDetails details) async {
-    if(controller.value.isInitialized) {
+    if (controller.value.isInitialized) {
       showFocusCircle = true;
       x = details.localPosition.dx;
       y = details.localPosition.dy;
@@ -143,7 +209,7 @@ class TakePictureScreenState extends State<TakePictureScreen> {
       double xp = x / fullWidth;
       double yp = y / cameraHeight;
 
-      Offset point = Offset(xp,yp);
+      Offset point = Offset(xp, yp);
       print("point : $point");
 
       // Manually focus
@@ -161,4 +227,5 @@ class TakePictureScreenState extends State<TakePictureScreen> {
       });
     }
   }
+
 }
