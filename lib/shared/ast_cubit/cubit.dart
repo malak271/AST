72a28f807d1f61d9b18f58362e2720973add588b
malak271 <malakghabayen396@gmail.com>
2023-05-11 10:08:12 +0300

    add sendUSerAdjustments function

--- lib/shared/ast_cubit/cubit.dart
+++ lib/shared/ast_cubit/cubit.dart
@@ -1,121 +1,229 @@
-
-import 'dart:io';
+import 'dart:convert';
 import 'dart:typed_data';
+import 'package:ast/models/adjustments_model.dart';
 import 'package:ast/models/ast_model.dart';
+import 'package:ast/models/user_tests_model.dart';
 import 'package:ast/shared/ast_cubit/states.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../network/remote/dio_helper.dart';
+import '../components/Constants.dart';
 
 class AppCubit extends Cubit<States> {
   AppCubit() : super(InitialState());
 
   static AppCubit getCubit(context) => BlocProvider.of(context);
 
+  UserTests? userTests;
+  void getUserTests(){
+    emit(GetTestsLoadingState());
+    DioHelper.getData(
+      url: 'user/tests',
+      onSuccess: (response) {
+        userTests=UserTests.fromJson(response.data);
+        emit(GetTestsSuccessState());
+      },
+      onError: (ApIError ) {
+        emit(GetTestsErrorState(ApIError.message.toString()));
+      },
+    );
 
-  ASTModel astModel =ASTModel(cropsDetails: []);
+  }
 
-  Uint8List croppedImg=Uint8List.fromList([]);
+  String? bacteria;
+  String? sampleType;
 
-  List<Uint8List> cropImages=[];
-
-  void uploadImage ({
+  String? image_path;
+  int? testId;
+  void createNewTest ({
+    required String bacteria,
+    required String sample_type,
     required String imagePath,
-
   }) async{
-    cropImages=[];
-
-    emit(UploadImgLoadingState(imagePath));
-
+    testId=0;
+    // image_path=image_path;
+    emit(CreateTestLoadingState());
     DioHelper.postData(
-        url: 'http://192.168.1.112:5000/api/cropimage',
+        url: 'test/create',
         data: {
-          // 'file':File(imagePath),
+          'bacteria': '$bacteria',
+          'sample_type': '$sample_type',
           'file':await MultipartFile.fromFile(imagePath)
-          // 'file':File('E:/graduation-project-library/AST-image-processing-master/tests/images/test0_crop')
         },
         onError: (ApIError) {
           print(ApIError.message);
-          emit(UploadImgErrorState(ApIError.message));
+          emit(CreateTestErrorState(ApIError.message));
         },
-        onSuccess: (response) {
-          print("success");
-
-          // print(response.data);
-
-          astModel=ASTModel.fromJson(response.data);
-
-          // astModel = (response.data as List)
-          //     .map((data) => ASTModel.fromJson(data))
-          //     .toList();
-
-          // astModel = ASTModel.fromJson(response.data);
-
-          // List<ASTModel> ll=[
-          //   ASTModel(radius: 10,label: "LEV5",centerX: 4,centerY: 5,diameter: 5),
-          //   ASTModel(radius: 10,label: "IMI10",centerX: 4,centerY: 5,diameter: 5),
-          //   ASTModel(radius: 10,label: "MRP10",centerX: 4,centerY: 5,diameter: 5)
-          // ];
-
-          // getCroppedImage(img_name: astModel!.cropsDetails![0].imgName??'', img_path: astModel!.cropsDetails![0].imgFolder??'');
-          emit(UploadImgSuccessState(astModel,imagePath));
+        onSuccess: (response)  {
+          if( response.data['Status']=="Sucess") {
+            testId=int.parse(response.data['test_id']);
+            emit(CreateTestSuccessState());
+          }else{
+            emit(CreateTestErrorState(response.data['Message']));
+          }
         });
   }
 
+  List images_id=[];
+  int numOfCrops=0;
+  void cropImage ({
+    required int test_id,
+  }) {
+    images_info.clear();
+    numOfCrops=0;
+    diameters.clear();
+    labels.clear();
+    emit(CropImageLoadingState());
+    DioHelper.postData(
+        url: 'process/crops',
+        data: {
+          'test_id': '$test_id',
+        },
+        onSuccess: (response)  {
+          print(".......$response");
+         numOfCrops= response.data[0]['Num of crops'];
+          print(".......");
+          if( numOfCrops > 0) {
+            images_id=response.data[1];
+            emit(CropImageSuccessState());
+          }else{
+            emit(CropImageErrorState(response.data['Message']));
+          }
+        } ,
+      onError: (ApIError) {
+      print(ApIError.message);
+      emit(CropImageErrorState(ApIError.message));
+    },);
+  }
 
+ List<ASTModel> images_info=[];
 
-  void getCroppedImage ({
-    required String img_name,
-    required String img_path,
-
-  }) async{
+ getOneCroppedImage ({
+    required int img_id,
+  }) {
 
     emit(GetImgLoadingState());
 
     DioHelper.postData(
         options:Options(responseType: ResponseType.bytes) ,
-        url: 'http://192.168.1.112:5000/sendimg',
+        url: 'fetch/crop',
         data: {
-          'img_name':img_name,
-          'img_path':img_path
+          'img_id':img_id,
         },
         onError: (ApIError) {
-          print(ApIError.message);
           emit(GetImgErrorState(ApIError.message));
         },
         onSuccess: (response) {
           // Get the image bytes from the response
           Uint8List imageBytes = Uint8List.fromList(response.data);
 
-          // Display the image in the app
-          // setState(() {
-          //   _image = Image.memory(imageBytes);
-          // });
+          Uint8List croppedImg = imageBytes ;
 
-          croppedImg = imageBytes ;
-          // cropImages.add(croppedImg);
-
-          if(croppedImg!=null)
-            emit(GetImgSuccessState(croppedImg));
-          else
-            emit(GetImgErrorState('no img in request'));
+          if(croppedImg.isNotEmpty) {
+            if(response.headers.value("atb-data") != null){
+              Map<String, dynamic> data=json.decode(response.headers.value("atb-data")!);
+              data['img']=croppedImg;
+              images_info.add(ASTModel.fromJson(data));
+              if(images_info.length != images_id.length) {
+                print("-------------------------------------------------");
+                print(images_info.length-1);
+                emit(NextState(images_info.length));
+              }
+              if(images_info.length == images_id.length) {
+                emit(GetImgSuccessState());
+              }
+            }
+          } else
+            emit(GetImgErrorState('The image id you provided doesn\'t match any stored image id'));
 
         });
   }
 
+ List<Map<String, dynamic>> adjustments=[];
+
+ sendUserAdjustments () {
+
+   print(labels);
+   print(diameters);
+   print(adjustments);
+
+
+    if(adjustments.isNotEmpty) {
+      emit(SendAdjLoadingState());
+      DioHelper.postData(
+          options: Options(responseType: ResponseType.bytes),
+          url: 'test/confirmation',
+          data: {
+            'test_id': testId.toString(),
+            'image_info': adjustments,
+          },
+          onError: (ApIError) {
+            print(ApIError.message);
+            emit(SendAdjErrorState(ApIError.message));
+          },
+          onSuccess: (response) {
+            print(response.data);
+            emit(SendAdjSuccessState());
+            // if (response.data['Status'] == "Success") {
+            //   emit(SendAdjSuccessState());
+            // }
+            // else
+            //   emit(SendAdjErrorState("TRY AGAIN"));
+          });
+    }else{
+      emit(NoAdjChangedState());
+    }
+  }
+
+  String interprateResults(int img_id){
+    if(img_id>415)
+      return "S";
+    return "R";
+  }
+
   PageController controller = PageController(initialPage: 0);
 
-  double radius= 0;
+  List<String> labels = [];
+  List<double> diameters = [];
 
+  void changeSlider(index,value) {
+    diameters[index]=value;
 
+    int result =adjustments.indexWhere((element) => element['"id"'] == images_info[index].imgId);
+    print(result);
+    if(result >= 0){
+      adjustments[result]["\"radius\""]=diameters[index];
+    }else{
+      AdjustmentModel model=AdjustmentModel(radius:diameters[index] ,id: images_info[index].imgId);
+      adjustments.add(model.toJson());
+    }
 
+    emit(ChangePageViewIndexState());
 
-  // int currentIndex = 0;
-  void changeSlider(value) {
-    radius = value;
+  }
+
+  void changeLabel(index,value){
+    labels[index]=value;
+    print("$index,$value");
+    print(labels);
+
+    // adjustments.clear();
+
+    int result =adjustments.indexWhere((element) => element['"id"'] == images_info[index].imgId);
+    print("ressss $result");
+    if(result >= 0){
+      adjustments[result]["\"label\""]='"'+labels[index]+'"';
+    }else{
+      AdjustmentModel model=AdjustmentModel(label:'"'+labels[index]+'"' ,id: images_info[index].imgId);
+      adjustments.add(model.toJson());
+    }
+
     emit(ChangePageViewIndexState());
   }
+
+  bool isChecked = false;
+
 
 
 
