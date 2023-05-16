d6cdc18e19fbb143cd25397605e03ab4427758aa
malak271 <malakghabayen396@gmail.com>
2023-05-16 10:22:22 +0300

    fix tojson function

--- lib/models/adjustments_model.dart
+++ lib/models/adjustments_model.dart
@@ -1,18 +1,21 @@
 class ResultModel{
   int? img_id;
   String? result;
+  String? label;
 
-  ResultModel({this.img_id, this.result});
+  ResultModel({this.img_id, this.result,this.label});
 
-  // ResultModel.fromJson(Map<String, dynamic> json) {
-  //   img_id = json['id'];
-  //   result = json['label'];
-  // }
+  ResultModel.fromJson(json) {
+    img_id = json['id'];
+    result = json['result'];
+    label= json['label'];
+  }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.img_id;
-    data['result'] = this.result;
+    data['"id"'] = this.img_id;
+    data['"result"'] = '"${this.result}"';
+    data['"label"']='"${this.label}"';
     return data;
   }
 }
